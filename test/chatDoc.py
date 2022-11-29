82e88f577e5a64d7c81c5d7b8fb65aab6c1a9f45
Ruuphoria <ruuphoria@proton.me>
2022-11-29 15:59:19 +0000

    Release candidate

--- test/chatDoc.py
+++ test/chatDoc.py
@@ -18,4 +18,6 @@ stable_llm_predictor = HuggingFaceLLMPredictor(
     tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
     model_name="StabilityAI/stablelm-tuned-alpha-3b"
 )
-service_context = ServiceContext.from_defaul
+service_context = ServiceContext.from_defaults(
+    chunk_size=1024, 
+    llm_predictor=stable_
